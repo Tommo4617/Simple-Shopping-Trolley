ee6c7327a2d84871d2d1a5665ccfec9ba906c1a2
Tommo4617 <AbbieT0406@gmail.com>
2025-07-31 17:45:44 +0100

    Completed the simple shopping trolley

---
+++ simple shopping cart/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_shopping_cart
+{
+    internal class Food
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
